6bca22613a94ad26598ab7547b3c2c0ae19d3d87
Rvor <nhamtybv@hotmail.com>
2015-12-22 07:53:04 +0000

    add user authenticate

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :name, presence: true, length: {:in => 6..20 }
+  validates :email, presence: true, uniqueness: true 
+  validates :password, presence: true, confirmation: true
+end
